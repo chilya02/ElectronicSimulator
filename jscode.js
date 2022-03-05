4aa61e7acecc216828e7dcdb644cbcb78818cdf3
ilya cherepanov <ilya.cherepanov.02@mail.ru>
2022-03-06 01:54:39 +0300

    Пофикшены баги с наведением мыши

--- jscode.js
+++ jscode.js
@@ -5,8 +5,11 @@ const LAYOUT_WIDTH = 100;
 const SCALE = 8;
 
 //colors:
+
 const MAIN_COLOR = '#808080'
-const SELECTED_COLOR = '#cccccc'
+const SELECTED_COLOR = '#00ffff'
+const NODE_COLOR = '#ffffff'
+const BACK_COLOR = '#000000'
 
 //Constants for Resistor params
 
@@ -25,73 +28,183 @@ const mainLampR = 3.4;
 const wireLampLength = ELEMENT_LENGTH / 2 - mainLampR;
 
 
+class Node {
 
-class Element {
-
-    constructor(x, y, orientation) {
-
-        this._orientation = orientation % 360;
+    constructor(){
+        this.x;
+        this.y;
         this.selected = false;
-        this._calcCoordinates(x, y);
-        this._calcPath();
-        this.draw();
+        this.update = false;
+    }
 
-    };
+    setPoint(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    checkPoint(xAbs, yAbs, isPressed){
+        
+        let x = Math.round((xAbs)/SCALE);
+        let y = Math.round((yAbs)/SCALE);
+
+        if (this.isInArea(x, y)){
+            if (!this.selected){this.selected = true}
+        } else {
+
+            if (this.selected & !isPressed){this.selected = false;}
+        }
+    }
+
+    isInArea(x, y){
+        return Math.abs(x - this.x) <= 5/SCALE & Math.abs(y - this.y) <= 5/SCALE
+    }
 
     set selected(value){
         this._selected = value;
-        if (this._selected){
-            ctx.strokeStyle = SELECTED_COLOR;
-            this.draw();
-            ctx.strokeStyle = MAIN_COLOR;
-        } else{
-            this.draw();
-        }
+        this.update = true;
     }
 
     get selected(){
         return this._selected
     }
 
-    isInArea(x, y){
-        
-        switch(this._orientation){
-            case 0:
-                return x > this.x1 & x < this.x2 & y == this.y1 & y == this.y2;
-
-            case 90:
-                return y < this.y1 & y > this.y2 & x == this.x1 & x == this.x2;
-            
-            case 180:
-                return x < this.x1 & x > this.x2 & y == this.y1 & y == this.y2;
-
-            case 270:
-                return y > this.y1 & y < this.y2 & x == this.x1 & x == this.x2;
-            
+    draw(ctx){
+        if (this.selected){
+            ctx.fillStyle = SELECTED_COLOR;
+            ctx.fillRect(this.x*SCALE - 5, this.y*SCALE - 5, 10, 10)
+            ctx.fillStyle = BACK_COLOR;
+        } else{
+            ctx.beginPath();
+            ctx.arc(this.x*SCALE, this.y*SCALE, 5, 0, 2*Math.PI);
+            ctx.fillStyle = NODE_COLOR;
+            ctx.fill();
+            ctx.fillStyle = BACK_COLOR;
         }
+    }
+}
+
+
+class Element {
+
+    constructor(x, y, orientation, ctx) {
+
+        this.x1 = x;
+        this.y1 = y;
+
+        this.xStart = this.x1;
+        this.yStart = this.y1;
         
+        this._node1 = new Node();
+        this._node2 = new Node();
+
+        this._orientation = orientation;
+        this._calcCoordinates();
+        this._calcPath();
+
+        this.selected = false;
+        this.update = false;
+        this.draw(ctx);
+    };
+
+    set _orientation(value){
+        while (value < 0){
+            value += 360;
+        }
+        if (value % 90 != 0){
+            console.log('Неправльный угол элемента')
+            return
+        } 
+        this.__orientation = value % 360;
     }
 
-    draw(){
+    get _orientation(){
+        return this.__orientation
+    }
+
+    set selected(value){
+        this._selected = value;
+        this.xStart = this.x1;
+        this.yStart = this.y1;
+        this.update = true;
+    }
+
+    get selected(){
+        return this._selected
+    }
+
+    stopDrag(){
+        this.xStart = this.x1;
+        this.yStart = this.y1;
+    }
+
+    isInArea(x, y){
+
+        let inNode = this._node1.isInArea(x, y) | this._node2.isInArea(x, y);
+        
+        switch(this._orientation){
+
+            case 0:
+                return x > this.x1 & x < this.x2 & y == this.y1 & y == this.y2 | inNode;
+
+            case 90:
+                return y < this.y1 & y > this.y2 & x == this.x1 & x == this.x2 | inNode;
+            
+            case 180:
+                return x < this.x1 & x > this.x2 & y == this.y1 & y == this.y2 | inNode;
+
+            case 270:
+                return y > this.y1 & y < this.y2 & x == this.x1 & x == this.x2 | inNode;
+            
+        }
+    }
+
+    checkPoint(xAbs, yAbs, xStart, yStart, isPressed){
+
+        this._node1.checkPoint(xAbs, yAbs, isPressed);
+        this._node2.checkPoint(xAbs, yAbs, isPressed);
+
+        let x = Math.round((xAbs)/SCALE);
+        let y = Math.round((yAbs)/SCALE);
+
+        if (this.isInArea(x, y)){
+            if (!this.selected){this.selected = true}
+        } else {
+
+            if (this.selected & !isPressed){this.selected = false;}
+        }
+
+        if (this.selected & isPressed){
+            this.move(Math.round((xAbs - xStart)/SCALE), Math.round((yAbs - yStart)/SCALE))
+        }
+
+        this.update = this.update | this._node1.update | this._node2.update;
+
+        this._node1.update = false;
+        this._node2.update = false;
+    }
+
+    draw(ctx){
         let element = new Path2D(this._path);
-        ctx.stroke(element);
+        if (this.selected){
+            ctx.strokeStyle = SELECTED_COLOR;
+            ctx.stroke(element);
+            ctx.strokeStyle = MAIN_COLOR;
+        } else {ctx.stroke(element)}
+
+        this._node1.draw(ctx);
+        this._node2.draw(ctx);
+
         console.log('DRAW')
     }
 
-    /*rotate(angle){
-        this._orientation += angle;
-        this._calcCoordinates();
-        let path = this._getPath();
-        this._draw(path);
-    }*/
+    _calcCoordinates(){  
 
-    _calcCoordinates(x, y){        
+        let x = this.x1;
+        let y = this.y1;    
 
         switch (this._orientation){
 
             case 0:
-                this.x1 = x;
-                this.y1 = y;
                 this.x2 = x + ELEMENT_LENGTH;
                 this.y2 = y;
                 this.centerX = x + ELEMENT_LENGTH / 2;
@@ -99,8 +212,6 @@ class Element {
             break;
 
             case 90:
-                this.x1 = x;
-                this.y1 = y;
                 this.x2 = x;
                 this.y2 = y - ELEMENT_LENGTH;
                 this.centerX = x;
@@ -108,8 +219,6 @@ class Element {
             break;
 
             case 180:
-                this.x1 = x;
-                this.y1 = y;
                 this.x2 = x - ELEMENT_LENGTH;
                 this.y2 = y;
                 this.centerX = x - ELEMENT_LENGTH / 2;
@@ -117,8 +226,6 @@ class Element {
             break;
 
             case 270:
-                this.x1 = x;
-                this.y1 = y;
                 this.x2 = x;
                 this.y2 = y + ELEMENT_LENGTH;
                 this.centerX = x;
@@ -126,15 +233,25 @@ class Element {
             break;
         }
 
+        this._node1.setPoint(this.x1, this.y1);
+        this._node2.setPoint(this.x2, this.y2);
+
         console.log(`x1: ${this.x1}, y1: ${this.y1}, x2: ${this.x2}, y2: ${this.y2}`)
     }
 
+    move(dx, dy){
+        this.x1 = this.xStart + dx;
+        this.y1 = this.yStart + dy;
+        this._calcCoordinates();
+        this._calcPath();
+        this.update = true;
+    }
 }
 
 
 class Resistor extends Element {
 
-    constructor(x, y, orientation) {super(x, y, orientation)}
+    constructor(x, y, orientation, ctx) {super(x, y, orientation, ctx)}
 
     _calcPath(){
         
@@ -202,13 +319,13 @@ class Resistor extends Element {
         switch (this._orientation % 180){
 
             case 0:
-                inBody = Math.abs(x - this.centerX) <= (mainResW / 2);
-                inBody = inBody & Math.abs(y - this.centerY) <= (mainResH / 2);
+                inBody = Math.abs(x - this.centerX) < (mainResW / 2);
+                inBody = inBody & Math.abs(y - this.centerY) < (mainResH / 2);
             break;
 
             case 90:
-                inBody = Math.abs(x - this.centerX) <= (mainResH / 2);
-                inBody = inBody & Math.abs(y - this.centerY) <= (mainResW / 2);
+                inBody = Math.abs(x - this.centerX) < (mainResH / 2);
+                inBody = inBody & Math.abs(y - this.centerY) < (mainResW / 2);
             break;
         }
 
@@ -219,7 +336,7 @@ class Resistor extends Element {
 
 class Key extends Element {
 
-    constructor(x, y, orientation) {super(x, y, orientation)}
+    constructor(x, y, orientation, ctx) {super(x, y, orientation, ctx)}
 
     _calcPath(){
 
@@ -302,7 +419,7 @@ class Key extends Element {
 
 class Lamp extends Element {
 
-    constructor(x, y, orientation) {super(x, y, orientation)}
+    constructor(x, y, orientation, ctx) {super(x, y, orientation, ctx)}
 
     _calcPath(){
 
@@ -378,7 +495,7 @@ class Lamp extends Element {
     isInArea(x, y){
         let distanceFromCenter = Math.sqrt(Math.pow(this.centerX - x, 2) + Math.pow(this.centerY - y, 2));
 
-        return super.isInArea(x, y) | distanceFromCenter <= mainLampR;
+        return super.isInArea(x, y) | distanceFromCenter < mainLampR;
     }
 }
 
@@ -390,29 +507,94 @@ class Wire extends Element {
     }
 }
 
-function canvasMove(e){
-    let x = Math.round((e.pageX - layout.offsetLeft)/SCALE);
-    let y = Math.round((e.pageY - layout.offsetTop)/SCALE);
-    for (let element of elements){
-        if (element.isInArea(x, y)){
-            element.selected = true;
-        } else if (element.selected){
-            element.selected = false;
-        }
+
+class Layout{
+
+    constructor(){
+        this.elements = [];
+        this.isPressed = false;
+        this.lastSelected = null;
+        this.xStart;
+        this.yStart;
+
+        this.canvas = document.getElementById('layout');
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx.lineWidth = 4;
+        this.ctx.strokeStyle = MAIN_COLOR;
+        this.ctx.fillStyle = BACK_COLOR;
+        this.ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+        
     }
+
+    checkPoint(xAbs, yAbs){
+        
+        let update = false;
+
+        if (this.lastSelected){
+            this.lastSelected.checkPoint(xAbs, yAbs, this.xStart, this.yStart, this.isPressed)
+
+            if (this.lastSelected.selected){
+
+                update = this.lastSelected.update;
+                this.lastSelected.update = false;
+
+
+            } else{ 
+                update = this._checkElements(xAbs, yAbs);            
+            }
+        } else {
+            update = this._checkElements(xAbs, yAbs);
+        }
+
+        
+        if (update)
+            this.invalidate(this.elements, this.ctx); 
+    }
+
+    _checkElements (xAbs, yAbs){
+        let update = false;
+        for (let element of this.elements){
+            
+            element.checkPoint(xAbs, yAbs, this.xStart, this.yStart, this.isPressed);         
+            update = update | element.update;
+            element.update = false;
+            if (element.selected) this.lastSelected = element;
+        }
+        return update;
+    }
+    
+    invalidate(){
+        this.ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+        for (let element of this.elements) element.draw(this.ctx);
+        if (this.lastSelected) this.lastSelected.draw(this.ctx);
+    }
+
 }
 
 
-let layout = document.getElementById('layout')
-let ctx = layout.getContext('2d');
 
-//layout.onclick = canvasClick;
-layout.onmousemove = canvasMove;
+function canvasMove(e){
+    let xAbs = e.pageX - layout.canvas.offsetLeft;
+    let yAbs = e.pageY - layout.canvas.offsetTop;
 
-ctx.fillStyle = "#000000";
-ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+    layout.checkPoint(xAbs,yAbs) 
+}
 
-ctx.lineWidth = 4;
-ctx.strokeStyle = MAIN_COLOR;
 
-let elements = [];
+function canvasRelease(e){
+    layout.isPressed = false;
+    layout.lastSelected.stopDrag();
+}
+
+function canvasClick(e){
+    layout.isPressed = true;
+    layout.xStart = e.pageX - layout.canvas.offsetLeft;
+    layout.yStart = e.pageY - layout.canvas.offsetTop;
+}
+
+
+let layout = new Layout();
+
+layout.canvas.onmousemove = canvasMove;
+layout.canvas.onmousedown = canvasClick;  
+layout.canvas.onmouseup = canvasRelease; 
